c1b4a0540a706c9886c10fbc6c0d30430b061469
Thiago Sartorel <thiagosartorel@uniplac.net>
2022-04-12 21:54:10 -0300

    Variaveis de ambiente + validação da entrada de energia e resposta do nobreak

--- server_nobreak.js
+++ server_nobreak.js
@@ -1,11 +1,10 @@
 const axios = require('axios');
-
-
+require('dotenv').config()
 
 const contatos = [
     "49999184629",
-    "49991108888",//49991108888
-    "49984030021"//49984030021
+    //"49991108888",//49991108888
+    //"49984030021"//49984030021
 ];
 const config_nobreaks =   [
     ["10443", 'no-break01', contatos],
@@ -16,17 +15,27 @@ const config_nobreaks =   [
 var auth = ''
 var battery = ''
 
-const url = 'http://10.3.0.34/zabbix/api_jsonrpc.php'
-
+const url = process.env.URL_ZABBIX
 
 config_nobreaks.forEach(item => {
     autentica(url).then((response) => {
+        var auth = response.data.result
         busca_battery_capacity(url, response.data.result, item[0])
         .then((response) => {
             //console.log(response.data.result[0].lastvalue);
             item[2].forEach(contato => {
                 if(response.data.result[0].lastvalue <= 98){
-                    var mensagem = "!!!Nivel do nobreak "+ item[1] +": " + response.data.result[0].lastvalue +"%";
+                    busca_voltage(url, auth, item[0])
+                    .then((response) => {
+                        //console.log(response.data.result[0].lastvalue);
+                        if(response.data.result[0].lastvalue == "0"){ // Valida se a entrada de eneria está zerada, se estiver envia mensagem
+                            var mensagem = "ALERTA!!!\mNivel do nobreak "+ item[1] +": " + response.data.result[0].lastvalue +"%";
+                            var destino = contato
+                            enviarmensagem(destino, mensagem)
+                        }
+                    })
+                }else if(response.data.result[0] === ''){// Se o nobreak não estiver retornando informações ele envia mensagens
+                    var mensagem = "Problema de conexão com o no break "+ item[1];
                     var destino = contato
                     enviarmensagem(destino, mensagem)
                 }
@@ -37,12 +46,13 @@ config_nobreaks.forEach(item => {
 })
 
 function enviarmensagem(destino, mensagem){
-    axios.post("http://127.0.0.1:8000/enviarmensagemjson", {
+    const url_mensagem = process.env.URL_ENVIA_MENSAGEM
+    axios.post(url_mensagem, {
             number:destino,
             message:mensagem,
             by:"alerta"
         })
- }
+}
 
 async function autentica(url_zabbix) {
     return await axios.post(url_zabbix, {
@@ -55,7 +65,6 @@ async function autentica(url_zabbix) {
         id: 1
     })
 }
-
 async function busca_battery_capacity(url_zabbix, auth, id_nobreak) {
         var bateriaGeradores =  await axios.post(url_zabbix, {
             jsonrpc: "2.0",
@@ -72,6 +81,24 @@ async function busca_battery_capacity(url_zabbix, auth, id_nobreak) {
             id: 1
         })
     return bateriaGeradores;
+}
+
+async function busca_voltage(url_zabbix, auth, id_nobreak) {
+    var voltagem =  await axios.post(url_zabbix, {
+        jsonrpc: "2.0",
+        method: "item.get",
+        params: {
+            output: "extend",
+            hostids: id_nobreak,
+            search: {
+                key_: "upsAdvOutputVoltage",
+            },
+            sortfield: "name"
+        },
+        auth: auth,
+        id: 1
+    })
+return voltagem;
 }
 //
 // autentica(url).then(function (response) {
